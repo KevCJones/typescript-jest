632a4dc38f20ffc86a039a7a6b062adba3f557d0
Kevin Jones <kevin.jones@ppushdr.com>
2020-10-05 19:53:05 +0100

    chore - fix test format

--- src/example/index.test.ts
+++ src/example/index.test.ts
@@ -3,12 +3,12 @@ import each from 'jest-each';
 
 import add1 from './index';
 
-describe('Example funtion - add1 function', () => {
-    describe(`add1`, () => {
+describe('Example function - add1 function', () => {
+    describe(`Calling add1`, () => {
       each([
         [1, 2],
         [-1, 0],
-      ]).test('with "%s" returns %s', (inputValue: number, expected: number) => {
+      ]).test('with %s returns %s', (inputValue: number, expected: number) => {
         expect(add1(inputValue)).toBe(expected);
       });
     });
